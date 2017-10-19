d5d8e9170a264ec22538700ce0537a6082a20e0c
Mohammed Chlieh <mohammed.chlieh@gmail.com>
2017-10-19 16:47:04 +0200

    installing sbt

--- script/provision_base.sh
+++ script/provision_base.sh
@@ -7,6 +7,15 @@ echo "Checking Docker version"
 docker -v 2>/dev/null || (echo "Installing Docker..." && curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh && docker -v)
 echo "Checking if Datadog is installed"
 /etc/init.d/datadog-agent info 2>/dev/null || (echo "Installing Datadog" && DD_API_KEY=877c66dee5181aff4932b479f44324f6 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)")
+echo "Upgrading sbt if possible"
+sbt_repo="deb https://dl.bintray.com/sbt/debian /"
+sbt_repo_file=/etc/apt/sources.list.d/sbt.list
+if [ ! -f  $sbt_repo_file ] || ! grep -q $sbt_repo $sbt_repo_file; then
+  echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+fi
+sudo apt-get update
+sudo apt-get install sbt
 echo "Checking ZSH version"
 # written for user ubuntu
 zsh --version 2>/dev/null || (sudo apt-get install -y zsh && sudo chsh -s /bin/zsh ubuntu && su ubuntu sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && cp /home/ubuntu/.oh-my-zsh/templates/zshrc.zsh-template /home/ubuntu/.zshrc && perl -pi.back -e 's/robbyrussell/blinks/g' /home/ubuntu/.zshrc && source /home/ubuntu/.zshrc)
